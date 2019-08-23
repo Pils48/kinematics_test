c9ec029d571da84891d1bf5ac570e299f59e3f27
Pils48 <ha.rg@yandex.ru>
2019-08-23 10:51:21 +0300

    Test trajectory segmentation, add exception in case invalid trajectory.
    Signed-off-by: Pils48 <ha.rg@yandex.ru>

--- src/kinematics_test.cpp
+++ src/kinematics_test.cpp
@@ -2,7 +2,7 @@
  * In this file we will test a kinematics algorithm trac-ik by
  * solving forward and inverse kinematic
  *********************************************************************/
- 
+
 #include <ros/ros.h>
 
 #include <vector>
@@ -24,7 +24,8 @@
 #include <moveit_visual_tools/moveit_visual_tools.h>
 
 #define STANDARD_INTERPOLATION_STEP 0.01
-#define EXPERIMENTAL_DISTANCE_CONSTRAINT 2
+#define EXPERIMENTAL_DISTANCE_CONSTRAINT 0.005
+#define EXPERIMENTAL_ATTEMPT_NUMBER 10
 
 using namespace std;
 using namespace moveit;
@@ -32,147 +33,128 @@ using namespace core;
 
 /** Interpolate trajectory using slerp quaternion algorithm and linear algorithms
  * for translation parameter. Return true in case of success.*/
-bool linearInterpolation(std::vector<robot_state::RobotStatePtr>& trail,
-        robot_state::RobotState kinematic_state, const Eigen::Affine3d& goal_transform, JumpThreshold& jump_threshold,
-        size_t translation_steps, bool global_reference_frame = true){
-
-    const robot_state::JointModelGroup* jmg = kinematic_state.getJointModelGroup("manipulator");
-//    std::vector<robot_state::RobotStatePtr> trail_copy = trail;
-    robot_state::RobotState current_state();
-    trail.push_back(robot_state::RobotStatePtr(new robot_state::RobotState(kinematic_state)));
-    const moveit::core::LinkModel* ptr_link_model = kinematic_state.getLinkModel("tool0");
-    // effector
-
-    Eigen::Affine3d start_pose = kinematic_state.getGlobalLinkTransform(ptr_link_model);
-
-    // the target can be in the local reference frame (in which case we rotate it)
-    Eigen::Affine3d rotated_target = global_reference_frame ? goal_transform : start_pose * goal_transform;
-
-    Eigen::Quaterniond start_quaternion(start_pose.rotation());
-    Eigen::Quaterniond target_quaternion(rotated_target.rotation());
-    double rotation_distance = start_quaternion.angularDistance(target_quaternion);
-    double translation_distance = (rotated_target.translation() - start_pose.translation()).norm();
-
-    std::size_t steps = translation_steps + 1;
-
-    std::vector<double> consistency_limits;
-    if (jump_threshold.prismatic > 0 || jump_threshold.revolute > 0)
-        for (const JointModel* jm : jmg->getActiveJointModels())
-        {
-            double limit;
-            switch (jm->getType())
-            {
-                case JointModel::REVOLUTE:
-                    limit = jump_threshold.revolute;
-                    break;
-                case JointModel::PRISMATIC:
-                    limit = jump_threshold.prismatic;
-                    break;
-                default:
-                    limit = 0.0;
-            }
-            if (limit == 0.0)
-                limit = jm->getMaximumExtent();
-            consistency_limits.push_back(limit);
-        }
-
-    double last_valid_percentage = 0.0;
-    for (std::size_t i = 1; i <= steps; ++i)
-    {
-        double percentage = (double)i / (double)steps;
-
-        Eigen::Affine3d pose(start_quaternion.slerp(percentage, target_quaternion));
-
-        pose.translation() = percentage * rotated_target.translation() + (1 - percentage) * start_pose.translation();
-
-        if (kinematic_state.setFromIK(jmg, pose, ptr_link_model->getName(), consistency_limits))
-            trail.push_back(robot_state::RobotStatePtr(new robot_state::RobotState(kinematic_state))); //tmp work
-            // with original
-        else{
-            ROS_ERROR("Impossible to create whole path! Check self-collision or limits excess.");
-            return false;
-        }
-        last_valid_percentage = percentage;
-    }
-//    last_valid_percentage *= CartesianInterpolator::checkJointSpaceJump(jmg, trail_copy,
-//            jump_threshold);
-
-    return true;
+bool linearInterpolation(vector<robot_state::RobotStatePtr>& trail,
+                         robot_state::RobotState kinematic_state, const Eigen::Affine3d& goal_transform,
+                         size_t translation_steps, bool global_reference_frame = true){
+	
+	const robot_state::JointModelGroup* jmg = kinematic_state.getJointModelGroup("manipulator");
+	vector<robot_state::RobotStatePtr> trail_copy(trail);
+	trail_copy.push_back(robot_state::RobotStatePtr(new robot_state::RobotState(kinematic_state)));
+	const moveit::core::LinkModel* ptr_link_model = kinematic_state.getLinkModel("tool0");
+	// effector
+	
+	Eigen::Affine3d start_pose = kinematic_state.getGlobalLinkTransform(ptr_link_model);
+	
+	// the target can be in the local reference frame (in which case we rotate it)
+	Eigen::Affine3d rotated_target = global_reference_frame ? goal_transform : start_pose * goal_transform;
+	
+	Eigen::Quaterniond start_quaternion(start_pose.rotation());
+	Eigen::Quaterniond target_quaternion(rotated_target.rotation());
+	double rotation_distance = target_quaternion.angularDistance(start_quaternion);
+	double translation_distance = (rotated_target.translation() - start_pose.translation()).norm();
+	
+	size_t steps = translation_steps + 1;
+	
+	for (size_t i = 1; i <= steps; ++i)
+	{
+		double percentage = (double)i / (double)steps;
+		
+		Eigen::Affine3d pose(start_quaternion.slerp(percentage, target_quaternion));
+		
+		pose.translation() = percentage * rotated_target.translation() + (1 - percentage) * start_pose.translation();
+		
+		if (kinematic_state.setFromIK(jmg, pose, ptr_link_model->getName()))
+			trail_copy.push_back(robot_state::RobotStatePtr(new robot_state::RobotState(kinematic_state)));
+		else{
+			ROS_ERROR("Impossible to create whole path! Check self-collision or limits excess.");
+			return false;
+		}
+		
+	}
+	trail = trail_copy;
+	return true;
 }
 
-map<string, double*> findLinksDistance(vector<robot_state::RobotStatePtr>& trail,
-        robot_state::RobotState& current_state, double critical_distance){
+bool isMoveValid(size_t state_idx, size_t next_state_idx, double critical_distance,
+                 vector<robot_state::RobotStatePtr>& trail){
+	
+	robot_state::RobotState current_state(trail[state_idx]->getRobotModel());
+	vector<robot_state::RobotStatePtr> segment_to_check(0);
+	bool is_interpolated = linearInterpolation(segment_to_check, current_state,
+	                                           trail[next_state_idx]->getGlobalLinkTransform("tool0"),1);
+	trail.insert(trail.begin() + next_state_idx, segment_to_check[1]);
+	return true;
+}
 
-    map<string, double*> result_distances;
+double getFullTranslation(const robot_state::RobotStatePtr state, const robot_state::RobotStatePtr next_state,
+                          Eigen::Vector3d& link_extends, string link_name){
+	const Eigen::Affine3d state_transform = state->getGlobalLinkTransform(link_name);
+	const Eigen::Affine3d next_state_transform = next_state->getGlobalLinkTransform(link_name);
+	Eigen::Quaterniond start_quaternion(state_transform.rotation());
+	Eigen::Quaterniond target_quaternion(next_state_transform.rotation());
+	//Find sin of angle between start_quaternion and target_quaternion
+	double sin_between_quaternions = 1 - pow(start_quaternion.dot(target_quaternion) / (start_quaternion.norm() *
+	                                                                                    target_quaternion.norm()), 2) ;
+	int x_coeff, y_coeff, z_coeff;
+	//Find which otctant
+	x_coeff = state_transform.translation()(0) > 0 ? 1 : -1;
+	y_coeff = state_transform.translation()(1) > 0 ? 1 : -1;
+	z_coeff = state_transform.translation()(2) > 0 ? 1 : -1;
+	
+	const Eigen::Translation3d origin_to_diagonal(x_coeff* link_extends[0] / 2, y_coeff * link_extends[1] / 2,
+	                                              z_coeff * link_extends[2] / 2);
+	double linear_angular_distance = (state_transform.translation() + origin_to_diagonal.translation()).norm() *
+	                                 sin_between_quaternions;
+	double dbg_value = (state_transform.translation() - next_state_transform.translation()).norm();
+	
+	return (state_transform.translation() - next_state_transform.translation()).norm() + linear_angular_distance;
+}
 
-    for (size_t idx = 1; idx <= 6; idx++){
-
-        string link_name = string("link_") + to_string(idx);
-        pair<string, double*> key_value;
-        key_value.first = link_name;
-        double link_distances[trail.size() - 1];
-        key_value.second = link_distances;
-
-        const LinkModel* ptr_link = current_state.getLinkModel(link_name);
-
-        //Work with the greatest translation of the link
-        const shapes::Shape* link_mesh = ptr_link->getShapes().at(0).get();
-        Eigen::Vector3d link_extends = shapes::computeShapeExtents(link_mesh);
-        Eigen::Affine3d mesh_origin_transform = ptr_link->getVisualMeshOrigin();
-
-        for (size_t i = 0; i < trail.size() - 1; i++){
-
-            Eigen::Affine3d state = trail[i]->getGlobalLinkTransform(link_name);
-            Eigen::Affine3d next_state = trail[i + 1]->getGlobalLinkTransform(link_name);
-
-            Eigen::Quaterniond start_quaternion(state.rotation());
-            Eigen::Quaterniond target_quaternion(next_state.rotation());
-            /**Quaternion start_quaternion --> target_quaternion
-             *target_quaternion = trans_quaternion * start_quaternion =>
-             *=> trans_quaternion = target_quaternion * start_quaternion^(-1)*/
-            Eigen::Quaterniond trans_quaternion = target_quaternion * start_quaternion.inverse();
-
-            /**Get the farthest point:
-             *take the point, that has a maximum distance to global origin, in case of different axis orientation
-             * introduce coefficients that will mean whether we should add or subtract half extend size
-            */
-            int x_coeff, y_coeff, z_coeff;
-            mesh_origin_transform(0, 0) > 0 ? x_coeff = 1 : x_coeff = -1;
-            mesh_origin_transform(1, 1) > 0 ? y_coeff = 1 : y_coeff = -1;
-            mesh_origin_transform(2, 2) > 0 ? z_coeff = 1 : z_coeff = -1;
-
-            Eigen::Vector3d diagonal_point_link = Eigen::Vector3d(x_coeff * link_extends(0) / 2,
-                    y_coeff * link_extends(1) / 2, z_coeff * link_extends(2) / 2);
-            Eigen::Vector3d diagonal_point_global = mesh_origin_transform * diagonal_point_link;
-            //Rotate vector(global_origin, diagonal_point_link)
-            Eigen::Vector3d end_point_global = trans_quaternion.toRotationMatrix() * diagonal_point_global;
-            double linear_angular_distance = (end_point_global - diagonal_point_global).norm();
-
-            double translation_distance = (next_state.translation() - state.translation()).norm() + linear_angular_distance;
-            key_value.second[i] = translation_distance;
-
-            ROS_INFO("%s the greatest translation distance: %f", link_name.c_str(), translation_distance);
-            if (translation_distance >= critical_distance){
-                ROS_WARN("%s seams to pass to great distance: %f", link_name.c_str(), translation_distance);
-                //We twice steps number
-                JumpThreshold jump_threshold;
-                jump_threshold.prismatic = 5.0;
-                jump_threshold.factor = 1.0;
-                jump_threshold.revolute = 1.0; //Random values
-                robot_state::RobotState kinematic_state = *trail[0];
-                const Eigen::Affine3d& goal_transform = trail[trail.size() - 1]->getGlobalLinkTransform("tool0");
-                size_t translation_steps = (trail.size() - 1) * 2;
-                trail.clear();
-                bool is_interpolated = linearInterpolation(trail, kinematic_state, goal_transform,jump_threshold,
-                        translation_steps);
-                findLinksDistance(trail, kinematic_state, critical_distance);
-            }
-        }
-
-        result_distances.insert(result_distances.end(), key_value);
-    }
-    return result_distances;
-};
+pair<string, vector<double> > findLinkDistance(vector<robot_state::RobotStatePtr>& trail,
+                                               const robot_state::LinkModel* link, double critical_distance,
+                                               size_t jump_state_idx = 0, size_t attempt = 1){
+	
+	pair<string, vector<double> > result_pair;
+	result_pair.first = link->getName();
+	vector<double> distances(0);
+	result_pair.second = distances;
+	
+	//Work with the greatest translation of the link
+	const shapes::Shape* link_mesh = link->getShapes().at(0).get(); //Refactor
+	Eigen::Vector3d link_extends = shapes::computeShapeExtents(link_mesh);
+	
+	for (size_t state_idx = 0; state_idx < trail.size() - 1; state_idx++){
+		
+		double translation_distance = getFullTranslation(trail[state_idx], trail[state_idx + 1],
+		                                                 link_extends, link->getName());
+		
+		if (attempt == EXPERIMENTAL_ATTEMPT_NUMBER){
+			ROS_ERROR("Space jump happened! Truncate trajectory after jump!");
+			return result_pair;
+		}
+		
+		while (translation_distance > critical_distance){
+			ROS_WARN("%s has to great translation: %f", link->getName().c_str(), translation_distance);
+			vector<robot_state::RobotStatePtr> segment_to_check(0);
+			bool is_interpolated = linearInterpolation(segment_to_check, *trail[state_idx], trail[state_idx + 1]
+					->getGlobalLinkTransform("tool0"), 1);
+			if (is_interpolated){
+				trail.insert(trail.begin() + state_idx + 1, segment_to_check[1]);
+				translation_distance = getFullTranslation(trail[state_idx], trail[state_idx + 1],
+				                                          link_extends, link->getName());
+			}
+			else{
+				ROS_ERROR("Space jump happened! Truncate trajectory after jump!");
+				throw Exception("Invalid trajectory!");
+			}
+			
+		}
+		result_pair.second.push_back(translation_distance);
+		ROS_INFO("%s translate : %f", link->getName().c_str(), translation_distance);
+	}
+	
+	return result_pair;
+}
 
 int main(int argc, char** argv)
 {
@@ -181,18 +163,18 @@ int main(int argc, char** argv)
 	ros::NodeHandle node_handle;
 	ros::AsyncSpinner spinner(1);
 	spinner.start();
-
+	
 	string PLANNING_GROUP = "manipulator";
 	moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
 	moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-
-    robot_model_loader::RobotModelLoader kt_robot_model_loader("robot_description");
-    robot_model::RobotModelConstPtr kinematic_model = kt_robot_model_loader.getModel();
-    planning_scene_monitor::PlanningSceneMonitor kt_planning_scene_monitor("robot_description");
-    planning_scene::PlanningScenePtr kt_planning_scene = kt_planning_scene_monitor.getPlanningScene();
-    robot_state::RobotState kinematic_state(kinematic_model);
+	
+	robot_model_loader::RobotModelLoader kt_robot_model_loader("robot_description");
+	robot_model::RobotModelConstPtr kinematic_model = kt_robot_model_loader.getModel();
+	planning_scene_monitor::PlanningSceneMonitor kt_planning_scene_monitor("robot_description");
+	planning_scene::PlanningScenePtr kt_planning_scene = kt_planning_scene_monitor.getPlanningScene();
+	robot_state::RobotState kinematic_state(kinematic_model);
 	ROS_INFO("Model frame: %s", kinematic_model->getModelFrame().c_str());
-
+	
 	kinematic_state.setToDefaultValues();
 	const robot_state::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("manipulator");
 	
@@ -200,66 +182,70 @@ int main(int argc, char** argv)
 	namespace rvt = rviz_visual_tools;
 	visual_tools.deleteAllMarkers();
 	visual_tools.loadRemoteControl();
-
-    Eigen::Affine3d text_pose = Eigen::Affine3d::Identity();
-    text_pose.translation().z() = 1.75;
-    visual_tools.publishText(text_pose, " Demo", rvt::WHITE, rvt::XLARGE);
-    visual_tools.trigger();
+	
+	Eigen::Affine3d text_pose = Eigen::Affine3d::Identity();
+	text_pose.translation().z() = 1.75;
+	visual_tools.publishText(text_pose, " Demo", rvt::WHITE, rvt::XLARGE);
+	visual_tools.trigger();
 
 //	visual_tools.prompt("Press next to continue execution...");
-    //end of initialization
-
+	//end of initialization
+	
 	/** Test trac-ik
 	kinematic_state.setToRandomPositions(joint_model_group);
 	const Eigen::Affine3d& end_effector_state = kinematic_state.getGlobalLinkTransform("tool0");
-	
+
 	//Visualization
 	visual_tools.publishRobotState(kinematic_state, rvt::colors::DARK_GREY);
 	visual_tools.prompt("Press next to continue execution...");
-	
-	std::vector<double> joint_values;
+
+	vector<double> joint_values;
 	double timeout = 0.1;
 	bool found_ik = kinematic_state.setFromIK(joint_model_group, end_effector_state, timeout);
-	
+
 	if(found_ik){
 		visual_tools.publishRobotState(kinematic_state, rvt::colors::BLUE);
 	}
 	visual_tools.prompt("Press next to continue execution...");
 
-    kinematic_state.setToDefaultValues(); */
+	kinematic_state.setToDefaultValues(); */
+	
+	const Eigen::Affine3d tool0_frame = kinematic_state.getGlobalLinkTransform("tool0");
+	
+	Eigen::Affine3d goal_transform(Eigen::Translation3d(-1, 0.7, 1.5)); //sx -1 sy 1 sz 1,5
+	const Eigen::Affine3d start_pose(Eigen::Translation3d(-1, 0.0, -0.3));
+	kinematic_state.setFromIK(joint_model_group, start_pose);
+	visual_tools.publishRobotState(kinematic_state, rvt::BLUE);
+	
+	vector<robot_state::RobotStatePtr> traj(0);
+	size_t approximate_steps = floor((goal_transform.translation() - start_pose.translation()).norm() /
+	                                 STANDARD_INTERPOLATION_STEP);
+	bool is_interpolated = linearInterpolation(traj, kinematic_state, goal_transform, approximate_steps);
+	
+	map<string, vector<double> > links_distances;
+	if (is_interpolated){
+		for (size_t link_idx = 1; link_idx <= 6; link_idx++){
+			string link_name = string("link_") + to_string(link_idx);
+			pair<string, vector<double> > link_distance_pair = findLinkDistance(traj, kinematic_state.getLinkModel(link_name),
+			                                                                    EXPERIMENTAL_DISTANCE_CONSTRAINT);
+			links_distances.insert(links_distances.end(), link_distance_pair);
+		}
+	}
+	
 
-	Eigen::Affine3d goal_transform(Eigen::Translation3d(1.0, 0.0, 0.4));
-    const Eigen::Affine3d start_pose(Eigen::Translation3d(1.0, 0.1, 0.3));
-    kinematic_state.setFromIK(joint_model_group, start_pose);
-    visual_tools.publishRobotState(kinematic_state, rvt::BLUE);
-
-    JumpThreshold jump_threshold;
-    jump_threshold.prismatic = 5.0;
-    jump_threshold.factor = 1.0;
-    jump_threshold.revolute = 1.0; //Random values
-    std::vector<robot_state::RobotStatePtr> traj(0);
-
-    size_t approximate_steps = floor((goal_transform.translation() - start_pose.translation()).norm() /
-            STANDARD_INTERPOLATION_STEP);
-    bool is_interpolated = linearInterpolation(traj, kinematic_state, goal_transform,
-            jump_threshold, approximate_steps);
-
-    robot_trajectory::RobotTrajectory trail(kinematic_model, joint_model_group);
-
-    map<string, double*> links_distances = findLinksDistance(traj, kinematic_state, EXPERIMENTAL_DISTANCE_CONSTRAINT);
-
+//    moveit_msgs::DisplayTrajectory traj_msg;
 //    moveit_msgs::RobotTrajectory traj_msg;
 //    trail.getRobotTrajectoryMsg(traj_msg);
 //    bool success = visual_tools.publishTrajectoryPath(trail);
 //    bool success = visual_tools.publishTrajectoryLine(traj_msg, joint_model_group, rvt::colors::GREEN);
 //    visual_tools.trigger();
-
-    //Visualize trajectory
-     for (std::size_t i = 0; i < traj.size(); i++){
-         std::this_thread::sleep_for(std::chrono::milliseconds(200));
-         visual_tools.publishRobotState(traj[i]);
-         std::this_thread::sleep_for(std::chrono::milliseconds(200));
-         visual_tools.deleteAllMarkers();
-     }
-
+	
+	//Visualize trajectory
+	for (size_t i = 0; i < traj.size(); i++){
+		this_thread::sleep_for(chrono::milliseconds(20));
+		visual_tools.publishRobotState(traj[i]);
+		this_thread::sleep_for(chrono::milliseconds(20));
+		visual_tools.deleteAllMarkers();
+	}
+	
 }
