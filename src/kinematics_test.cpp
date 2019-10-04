41c3cbb09a56be5e6b8b07cf2e5cc9d50bfe6c31
npilyugin <npilyugin@abagy.com>
2019-10-04 16:48:42 +0300

    Remove unexpected trajectory execution. TO DO: 1) checlJumpTemplate work improperly. 2) Add abstract factory, by interface or template.
    Signed-off-by: npilyugin <npilyugin@abagy.com>

--- src/kinematics_test.cpp
+++ src/kinematics_test.cpp
@@ -31,58 +31,48 @@ using namespace moveit;
 using namespace core;
 using namespace Eigen;
 
-static constexpr double CONTINUITY_CHECK_THRESHOLD = M_PI * 0.0001;
+static constexpr double CONTINUITY_CHECK_THRESHOLD = M_PI * 0.001;
 static const double ALLOWED_COLLISION_DEPTH = 0.0000001;
 static const double LINEAR_TARGET_PRECISION = 0.005;
 static const double STANDARD_INTERPOLATION_STEP = 0.01;
 
-struct RobotPosition
-{
-    RobotState& base_state;
+
+struct RobotPosition {
+    RobotState &base_state;
     tf::Transform robot_pose;
 };
 
-class TransformSlerper
-{
+class TransformSlerper {
 public:
     TransformSlerper(const tf::Transform &source, const tf::Transform &target)
-            : source(source)
-            , target(target)
-            , source_rotation(source.getRotation())
-            , target_rotation(target.getRotation())
-            , total_distance(target.getOrigin().distance(source.getOrigin()))
-            , total_angle(source_rotation.angleShortestPath(target_rotation))
-    {}
+            : source(source), target(target), source_rotation(source.getRotation()),
+              target_rotation(target.getRotation()), total_distance(target.getOrigin().distance(source.getOrigin())),
+              total_angle(source_rotation.angleShortestPath(target_rotation)) {}
 
-    tf::Transform slerpByPercentage(double percentage) const
-    {
+    tf::Transform slerpByPercentage(double percentage) const {
         return tf::Transform(
                 source_rotation.slerp(target_rotation, percentage),
                 percentage * target.getOrigin() + (1 - percentage) * source.getOrigin()
         );
     }
 
-    tf::Transform slerpByDistance(double distance) const
-    {
+    tf::Transform slerpByDistance(double distance) const {
         if (total_distance < std::numeric_limits<double>::epsilon())
             return source;
         return slerpByPercentage(distance / total_distance);
     }
 
-    tf::Transform slerpByAngle(double angle) const
-    {
+    tf::Transform slerpByAngle(double angle) const {
         if (total_angle < std::numeric_limits<double>::epsilon())
             return source;
         return slerpByPercentage(angle / total_angle);
     }
 
-    double totalDistance() const
-    {
+    double totalDistance() const {
         return total_distance;
     }
 
-    double totalAngle() const
-    {
+    double totalAngle() const {
         return total_angle;
     }
 
@@ -94,65 +84,53 @@ private:
     double total_angle;
 };
 
-class PoseAndStateInterpolator
-{
+class PoseAndStateInterpolator {
 public:
     PoseAndStateInterpolator(
             const tf::Transform &source,
             const tf::Transform &target,
             const RobotState &start_state,
             const RobotState &end_state
-            ) : _slerper(source, target)
-            , _start_state(start_state)
-            , _end_state(end_state)
-    {
+    ) : _slerper(source, target), _start_state(start_state), _end_state(end_state) {
     }
 
     PoseAndStateInterpolator(
-            const Isometry3d& source,
-            const Isometry3d& target,
-            const RobotState& start_state,
-            const RobotState& end_state
-            ) : _start_state(start_state)
-            , _end_state(end_state)
-            , _slerper(paramToTF(source), paramToTF(target))
-    {
+            const Isometry3d &source,
+            const Isometry3d &target,
+            const RobotState &start_state,
+            const RobotState &end_state
+    ) : _start_state(start_state), _end_state(end_state), _slerper(paramToTF(source), paramToTF(target)) {
     }
 
-    tf::Transform paramToTF(const Isometry3d& source){
+    tf::Transform paramToTF(const Isometry3d &source) {
         tf::Transform res_pose;
         tf::poseEigenToTF(source, res_pose);
         return res_pose;
     }
 
-    void interpolateByPercentage(double percentage, tf::Transform &pose, RobotState &state)
-    {
+    void interpolateByPercentage(double percentage, tf::Transform &pose, RobotState &state) {
         pose = _slerper.slerpByPercentage(percentage);
         _start_state.interpolate(_end_state, percentage, state);
         state.update();
     }
 
-    void interpolateByDistance(double distance, tf::Transform &pose, RobotState &state) const
-    {
+    void interpolateByDistance(double distance, tf::Transform &pose, RobotState &state) const {
         pose = _slerper.slerpByDistance(distance);
         _start_state.interpolate(_end_state, distance / totalDistance(), state);
         state.update();
     }
 
-    void interpolateByAngle(double angle, tf::Transform &pose, RobotState &state) const
-    {
+    void interpolateByAngle(double angle, tf::Transform &pose, RobotState &state) const {
         pose = _slerper.slerpByAngle(angle);
         _start_state.interpolate(_end_state, angle / totalAngle(), state);
         state.update();
     }
 
-    double totalDistance() const
-    {
+    double totalDistance() const {
         return _slerper.totalDistance();
     }
 
-    double totalAngle() const
-    {
+    double totalAngle() const {
         return _slerper.totalAngle();
     }
 
@@ -162,46 +140,49 @@ private:
     const RobotState &_end_state;
 };
 
-struct LinearParams{
-    const JointModelGroup* group;
-    const LinkModel* end_effector;
+struct LinearParams {
+    const JointModelGroup *group;
+    const LinkModel *end_effector;
     const double interpolation_step;
 };
 
-class TestIKSolver{
+class TestIKSolver {
 public:
-    bool setStateFromIK(const LinearParams& params, tf::Transform& pose, RobotState& state)
-    {
+    bool setStateFromIK(const LinearParams &params, tf::Transform &pose, RobotState &state) {
         Isometry3d eigen_pose;
         tf::poseTFToEigen(pose, eigen_pose);
-        return state.setFromIK(params.group, eigen_pose);
+        return state.setFromIK(params.group, eigen_pose, 0.0, [](RobotState* robot_state, const JointModelGroup* joint_group,
+                const double* joint_group_variable_values){
+            return true;
+        });
     }
 };
 
-template <typename Interpolator>
-Interpolator getInterpolator(const LinearParams& params, RobotState& left, RobotState& right)
-{
+template<typename Interpolator>
+Interpolator* getInterpolator(const LinearParams &params, RobotState &left, RobotState &right) {
     tf::Transform left_pose;
     tf::Transform right_pose;
     tf::poseEigenToTF(left.getGlobalLinkTransform(params.end_effector), left_pose);
     tf::poseEigenToTF(right.getGlobalLinkTransform(params.end_effector), right_pose);
-    Interpolator interpolator(left_pose, right_pose, left, right);
-    return interpolator;
+    return new Interpolator(left_pose, right_pose, left, right);
 }
 
-template <typename Interpolator>
-RobotState getMidState(const LinearParams& params, RobotState& start_state, RobotState& end_state)
-{
-    tf::Transform pose;
-    RobotState result(start_state);
+template<typename Interpolator>
+tf::Transform getMidState(const LinearParams &params, RobotState& left, RobotState& right) {
+    tf::Transform res_pose;
+    RobotState result(left);
+    tf::Transform left_pose;
+    tf::Transform right_pose;
+    tf::poseEigenToTF(left.getGlobalLinkTransform(params.end_effector), left_pose);
+    tf::poseEigenToTF(right.getGlobalLinkTransform(params.end_effector), right_pose);
+    PoseAndStateInterpolator interpolator(left_pose, right_pose, left, right);
     //Use interface to make abstract factory??
-    auto interpolator = getInterpolator<PoseAndStateInterpolator>(params, start_state, end_state);
-    interpolator.interpolateByPercentage(0.5, pose, result);
-    return result;
+//    Interpolator interpolator = getInterpolator<Interpolator>(params, left, right);
+    interpolator.interpolateByPercentage(0.5, res_pose, result);
+    return res_pose;
 }
 
-double getFullTranslation(RobotState& state, RobotState& next_state, string link_name)
-{
+double getFullTranslation(RobotState &state, RobotState &next_state, string link_name) {
     auto link_mesh_ptr = state.getLinkModel(link_name)->getShapes()[0].get();
     Vector3d link_extends = shapes::computeShapeExtents(link_mesh_ptr);
 
@@ -218,42 +199,48 @@ double getFullTranslation(RobotState& state, RobotState& next_state, string link
     return (state_transform.translation() - next_state_transform.translation()).norm() + linear_angular_distance;
 }
 
-double getMaxTranslation(RobotState& state, RobotState& next_state){
+double getMaxTranslation(RobotState &state, RobotState &next_state) {
     double max_dist = 0;
     for (auto link_name : state.getJointModelGroup(PLANNING_GROUP)->getUpdatedLinkModelsWithGeometryNames())
         max_dist = max(max_dist, getFullTranslation(state, next_state, link_name));
     return max_dist;
 }
 
-template <typename Interpolator, typename IKSolver, typename OutputIterator>
-bool linearInterpolationTemplate(const LinearParams& params, const RobotState& base_state, Interpolator&& interpolator,
-        IKSolver&& solver, size_t steps, OutputIterator out)
-{
+template<typename Interpolator, typename IKSolver, typename OutputIterator>
+bool linearInterpolationTemplate(const LinearParams &params, const RobotState &base_state, Interpolator &&interpolator,
+                                 IKSolver &&solver, size_t steps, OutputIterator out) {
     RobotState current(base_state);
     *out++ = current;
     tf::Transform pose;
     for (size_t i = 1; i <= steps; ++i) {
         double percentage = (double) i / (double) steps;
-        interpolator.interpolateByPercentage(percentage, pose, current);
+        interpolator->interpolateByPercentage(percentage, pose, current);
+        if (!solver.setStateFromIK(params, pose, current)){
+            return false;
+        }
         *out++ = current;
     }
     return true;
 }
 
-template <typename OutputIterator, typename Interpolator>
-size_t splitTrajectoryTemplate(OutputIterator out, Interpolator&& interpolator, const LinearParams& params,
-        RobotState& left, RobotState& right)
-{
+template<typename OutputIterator, typename Interpolator, typename IKSolver>
+size_t splitTrajectoryTemplate(OutputIterator out, Interpolator &&interpolator, IKSolver&& solver, const LinearParams &params,
+                               RobotState left, RobotState right) {
     size_t segments;
+    tf::Transform mid_pose;
     RobotState mid(right);
     deque<RobotState> state_stack;
     vector<RobotState> buffer;
     state_stack.push_back(mid);
-    while (!state_stack.empty()){
+    while (!state_stack.empty()) {
         right = state_stack.back();
-        mid = getMidState<Interpolator>(params, left, right);
-        if (getMaxTranslation(left, right) >= LINEAR_TARGET_PRECISION)
+        mid_pose = getMidState<PoseAndStateInterpolator>(params, left, right);
+        if (!solver.setStateFromIK(params, mid_pose, mid)){
+            throw runtime_error("Invalid trajectory!");
+        }
+        if (getMaxTranslation(left, right) >= LINEAR_TARGET_PRECISION){
             state_stack.push_back(mid);
+        }
         else {
             buffer.push_back(mid);
             state_stack.pop_back();
@@ -265,25 +252,25 @@ size_t splitTrajectoryTemplate(OutputIterator out, Interpolator&& interpolator, 
     return segments;
 }
 
-template <typename Interpolator, typename IKSolver>
-bool checkJumpTemplate(const LinearParams& params, Interpolator&& interpolator, IKSolver&& solver,
-                RobotState left, RobotState right)
-{
+template<typename Interpolator, typename IKSolver>
+bool checkJumpTemplate(const LinearParams &params, Interpolator&& interpolator, IKSolver &&solver,
+                       RobotState left, RobotState right) {
     tf::Transform mid_pose;
     RobotState mid(left);
     auto dist = left.distance(right);
     double percentage = 1;
-    while (percentage >= 0.00005 && dist > CONTINUITY_CHECK_THRESHOLD){
+    while (percentage >= 0.00005 && dist > CONTINUITY_CHECK_THRESHOLD) {
         percentage *= 0.5;
-        mid = getMidState<Interpolator>(params, left, right);
+        mid_pose = getMidState<Interpolator>(params, left, right);
+        if (!solver.setStateFromIK(params, mid_pose, mid)){
+            throw runtime_error("Invalid trajectory!");
+        }
         auto lm = left.distance(mid);
         auto mr = mid.distance(right);
-        if (lm < mr){
+        if (lm < mr) {
             left = mid;
             dist = mid.distance(right);
-        }
-        else
-        {
+        } else {
             right = mid;
             dist = left.distance(mid);
         }
@@ -294,8 +281,7 @@ bool checkJumpTemplate(const LinearParams& params, Interpolator&& interpolator, 
     return true;
 }
 
-void checkAllowedCollision(RobotState& state, planning_scene::PlanningScenePtr current_scene)
-{
+void checkAllowedCollision(RobotState &state, planning_scene::PlanningScenePtr current_scene) {
     collision_detection::CollisionRequest req;
     collision_detection::CollisionResult res;
     current_scene->checkCollision(req, res, state);
@@ -310,21 +296,18 @@ void checkAllowedCollision(RobotState& state, planning_scene::PlanningScenePtr c
         double contact_depth = contact_vector[0].depth;
         if (contact_depth > ALLOWED_COLLISION_DEPTH)
             throw runtime_error("Collision during the trajectory processing!\nInvalid trajectory!");
-    }
-    else
+    } else
         throw runtime_error("Collision during the trajectory processing!\nInvalid trajectory!");
 }
 
-void checkCollision(list<RobotStatePtr> trajectory, planning_scene::PlanningScenePtr current_scene)
-{
-    for (auto state_it = next(trajectory.begin()); state_it != prev(trajectory.end()); ++state_it){
+void checkCollision(list<RobotStatePtr> trajectory, planning_scene::PlanningScenePtr current_scene) {
+    for (auto state_it = next(trajectory.begin()); state_it != prev(trajectory.end()); ++state_it) {
         if (current_scene->isStateColliding(**state_it, PLANNING_GROUP, true))
             throw runtime_error("Collision during the trajectory processing!\nInvalid trajectory!");
     }
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
     //Initialization
     ros::init(argc, argv, "kinematics_test");
     ros::NodeHandle node_handle;
@@ -341,7 +324,7 @@ int main(int argc, char** argv)
     ROS_INFO("Model frame: %s", kt_kinematic_model->getModelFrame().c_str());
 
     kt_kinematic_state.setToDefaultValues();
-    const JointModelGroup* joint_model_group_ptr = kt_kinematic_model->getJointModelGroup(PLANNING_GROUP);
+    const JointModelGroup *joint_model_group_ptr = kt_kinematic_model->getJointModelGroup(PLANNING_GROUP);
 
     moveit_visual_tools::MoveItVisualTools visual_tools(kt_kinematic_model->getModelFrame());
     namespace rvt = rviz_visual_tools;
@@ -349,8 +332,8 @@ int main(int argc, char** argv)
     visual_tools.loadRemoteControl();
     //end of initialization
 
-    tf::Transform tf_start;
-    tf::Transform tf_goal(tf::createQuaternionFromRPY(0, M_PI_2, 0), tf::Vector3(1.185, 0, 1.565));
+    tf::Transform tf_start(tf::createQuaternionFromRPY(0, 0, 0), tf::Vector3(1.085, 0, 1.565));
+    tf::Transform tf_goal(tf::createQuaternionFromRPY(0, M_PI_4, 0), tf::Vector3(1.185, 0, 1.565));
     Isometry3d start_transform;
     Isometry3d goal_transform;
     tf::transformTFToEigen(tf_goal, goal_transform);
@@ -365,24 +348,25 @@ int main(int argc, char** argv)
     RobotState goal_state(kt_kinematic_model);
     RobotState start_state(kt_kinematic_model);
     goal_state.setFromIK(joint_model_group_ptr, goal_transform);
-    start_state.setToDefaultValues();
-    tf::transformEigenToTF(start_state.getGlobalLinkTransform(FANUC_M20IA_END_EFFECTOR), tf_start);
+    start_state.setFromIK(joint_model_group_ptr, start_transform);
 
     list<RobotState> trajectory;
     auto out = back_inserter(trajectory);
-    LinearParams params = {joint_model_group_ptr, joint_model_group_ptr->getLinkModel(FANUC_M20IA_END_EFFECTOR), STANDARD_INTERPOLATION_STEP};
+    LinearParams params = {joint_model_group_ptr, joint_model_group_ptr->getLinkModel(FANUC_M20IA_END_EFFECTOR),
+                           STANDARD_INTERPOLATION_STEP};
     TestIKSolver solver;
     size_t approximate_steps = floor(getMaxTranslation(start_state, goal_state) / STANDARD_INTERPOLATION_STEP);
-    PoseAndStateInterpolator interpolator(tf_start, tf_goal, start_state, goal_state);
-    linearInterpolationTemplate(params, kt_kinematic_state, interpolator, solver, (++approximate_steps), out);
-    for (auto state_it = trajectory.begin(); state_it != prev(trajectory.end()); ++state_it)
-    {
+    PoseAndStateInterpolator* interpolator = new PoseAndStateInterpolator(tf_start, tf_goal, start_state, goal_state);
+    linearInterpolationTemplate(params, start_state, interpolator, solver, approximate_steps, out);
+    for (auto state_it = trajectory.begin(); state_it != prev(trajectory.end()); ++state_it) {
         auto insert_it = inserter(trajectory, next(state_it));
         if (getMaxTranslation(*state_it, *next(state_it)) > LINEAR_TARGET_PRECISION)
-            advance(state_it, splitTrajectoryTemplate(insert_it, interpolator, params, *state_it, *next(state_it)));
+            if (!checkJumpTemplate(params, interpolator, solver, *state_it, *next(state_it)))
+                throw runtime_error("Invalid trajectory!");
+            advance(state_it, splitTrajectoryTemplate(insert_it, interpolator, solver, params, *state_it, *next(state_it)));
     }
 
-    for (auto it = trajectory.begin(); it != trajectory.end(); ++it){
+    for (auto it = trajectory.begin(); it != trajectory.end(); ++it) {
         this_thread::sleep_for(chrono::milliseconds(1));
         visual_tools.publishRobotState(*it);
         this_thread::sleep_for(chrono::milliseconds(1));
